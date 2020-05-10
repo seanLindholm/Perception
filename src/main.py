e6c173b77e565bd0f80a67ccb69763f156fe5f99
Oliver <oliverbehrens@hotmail.com>
2020-05-10 08:50:30 +0200

    up

--- src/main.py
+++ src/main.py
@@ -6,17 +6,18 @@ import os
 import time
 import sys 
 
-background = cv2.imread("C:\\Users\\swang\\Desktop\\Video\\NoOcclusions\\left\\1585434282_261431932_Left.png")
+background = cv2.imread("ImageTracking/left/1585434282_261431932_Left.png")
+#background = cv2.imread("ImageTracking/Stereo_conveyor_with_occlusions/left/1585434751_931764603_Left.png")
 
 
 
 def runVideoStreamNoOcculison():
     # Get the number of images in the left folder (The Video)
     # There is the same number of images in both left and right folder.
-    path_left = "C:\\Users\\swang\\Desktop\\Video\\NoOcclusions\\left"
-    path_right = "C:\\Users\\swang\\Desktop\\Video\\NoOcclusions\\right"
+    path_left = "ImageTracking/left"
+    path_right = "ImageTracking/right"
     
-    left_pics = [f for f in os.listdir(path_left)]
+    left_pics = sorted([f for f in os.listdir(path_left)])
     right_pics = [f for f in os.listdir(path_right)]
     num_pics = len(left_pics)
     
@@ -24,7 +25,8 @@ def runVideoStreamNoOcculison():
     # This creates and object, where (for each time we call it)
     # We get the next element of the loop. The generator is created
     # using yield, instead of return
-
+    
+    
     for i in range(num_pics):
         left  = cv2.imread(path_left+"/"+left_pics[i])
         right = cv2.imread(path_right+"/"+right_pics[i])
@@ -35,11 +37,12 @@ def runVideoStreamNoOcculison():
 def runVideoStreamWithOccultions():
      # Get the number of images in the left folder (The Video)
     # There is the same number of images in both left and right folder.
-    path_left = "C:\\Users\swang\Desktop\Video\Occlusions\left"
-    path_right = "C:\\Users\swang\Desktop\Video\Occlusions\\right"
+    path_left = "ImageTracking/Stereo_conveyor_with_occlusions/left"
+    path_right = "ImageTracking/Stereo_conveyor_with_occlusions/right"
     
     left_pics = [f for f in os.listdir(path_left)]
     right_pics = [f for f in os.listdir(path_right)]
+    left_pics = sorted(left_pics)
     num_pics = len(left_pics)
     
     # Here we can now make use of what is known as generators
@@ -91,7 +94,10 @@ def seqmentObject(img):
 if __name__ == "__main__":
     test = False
     model = ClassifyImages(load_model=True,dataset = "./ImageTracking/dataset.csv", load_path="./ImageTracking/model_save")
-        
+    
+    #img = cv2.imread("./images/book_00004.jpg")
+    #print("classification: ",t.classify_img(img))
+
     if not test:
         
         
@@ -99,36 +105,59 @@ if __name__ == "__main__":
 
         
         font                   = cv2.FONT_HERSHEY_SIMPLEX
-        bottomLeftCornerOfText = (10,50)
+        bottomLeftCornerOfText = (10,500)
         fontScale              = 1
         fontColor              = (255,255,255)
         lineType               = 2
         frame = 0
-        
+
+        Count = np.zeros(3)
         for left,right in video:
+            cl = "0"
+            mean = []
             frame += 1
             mask_object,boarder,mean = seqmentObject(left)
             #class_ = model.classify_img(mask_object,True)
             #contours
-            left = cv2.bitwise_and(left,left,mask=mask_object)
+            #left = cv2.bitwise_and(left,left,mask=mask_object)
+            print(mean)
             if boarder != []:
-                c_img = left[mean[0]-80:mean[0]+80,mean[1]-80:mean[1]+80,:]
+                c_img = left[mean[0]-120:mean[0]+120,mean[1]-120:mean[1]+120,:]
                 cv2.rectangle(left, boarder[0][0], boarder[0][1], (0, 255, 0), 2)
-                class_,prob,_ = model.classify_img(c_img,False)              
-                cv2.putText(left,class_ + " - " + str(frame) + " prob: " + str(prob), 
-                bottomLeftCornerOfText, 
-                font, 
-                fontScale,
-                fontColor,
-                lineType)
-                cv2.imshow("Crop",c_img)
+                if mean[1]<1050:
+                    #c_img = clone[y:y+winH, x:x+winW,:]
+                    cv2.imshow("Crop",c_img)
+                    cl = str(model.classify_img(c_img,False))
+                    cll = model.classify_img(c_img,False)
+                    if cll[0] == "cup":
+                        Count[0] = Count[0]+cll[1]*1
+                    elif cll[0] == "box":
+                        Count[2] = Count[2]+ cll[1]*1
+                    elif cll[0] =="book":
+                        Count[1] = Count[1] + cll[1]*1
+                    cl = model.categoryDict[np.argmax(Count)]
+                elif mean[1]>1050: 
+                    Count = np.zeros(3)
+            print(Count)
+                
+
+                    
+                    
+
+
+            cv2.putText(left,cl + " - " + str(frame), 
+            bottomLeftCornerOfText, 
+            font, 
+            fontScale,
+            fontColor,
+            lineType)
             cv2.imshow("left",left)
-           # cv2.imshow("left",left)
-           # cv2.imshow("image to class",classImg)
+            #cv2.imshow("image to class",classImg)
             cv2.waitKey(1)
             #cv2.destroyAllWindows()
+            
     else:
-
+        
         test = TestConvSeq()
         for img in test:
             img = primeImgForClassification(img,True)
